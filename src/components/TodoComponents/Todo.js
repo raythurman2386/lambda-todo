c8c3afdf04964043c8393ebce7e1dc271b5b296e
raythurman2386 <raymondthurman5@gmail.com>
2019-09-28 12:36:42 -0400

    adding placeholder todos to screen

---
+++ src/components/TodoComponents/Todo.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Todo = ({ todo }) => {
+  return (
+    <div>
+      <h4>{todo.task}</h4>
+    </div>
+  )
+}
+
+export default Todo
--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,10 +1,14 @@
 // your components will all go in this `component` directory.
 import React from 'react'
 
-const TodoList = () => {
+// component
+import Todo from './Todo'
+
+const TodoList = ({ todos }) => {
   return (
     <div>
       <h3>Todo List Component</h3>
+      {todos && todos.map(todo => <Todo todo={todo} />)}
     </div>
   )
 }
