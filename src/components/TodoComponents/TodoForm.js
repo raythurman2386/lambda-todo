fbfb6bd357c8c8ca99fc7c5001e6c2b037122150
Raymond Thurman <raymondthurman5@gmail.com>
2019-09-30 12:21:59 +0000

    minor form styles

--- src/components/TodoComponents/TodoForm.js
+++ src/components/TodoComponents/TodoForm.js
@@ -1,8 +1,9 @@
 import React from 'react'
+import styled from 'styled-components'
 
 const TodoForm = ({ todo, handleChange, handleSubmit, handleDelete }) => {
   return (
-    <>
+    <FormWrapper>
       <form onSubmit={handleSubmit}>
         <input
           name='todo'
@@ -13,8 +14,14 @@ const TodoForm = ({ todo, handleChange, handleSubmit, handleDelete }) => {
         <button type='submit'>Add Todo</button>
       </form>
       <button onClick={() => handleDelete()}>Clear Todos</button>
-    </>
+    </FormWrapper>
   )
 }
 
 export default TodoForm
+
+const FormWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
